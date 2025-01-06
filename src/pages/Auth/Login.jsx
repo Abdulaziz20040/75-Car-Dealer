970c7ce8c7199cbadac3a7ba2626e4143a56b726
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-01-06 11:10:59 +0500

    add login && backend

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,131 +1,192 @@
 import React, { useState } from "react";
-import { FaFacebookF, FaGoogle, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [isRegister, setIsRegister] = useState(false);
   const [error, setError] = useState("");
+  const [open, setopen] = useState(false);
 
   const handleToggle = () => {
     setIsRegister(!isRegister);
     setError("");
-    setUsername("");
+    setEmail("");
     setPassword("");
+    setFirstName("");
+    setLastName("");
+    setPhone("");
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmitRegister = async (e) => {
     e.preventDefault();
-
-    if (!username || !password) {
-      setError("Iltimos ma'lumotlarni to'ldiring");
+    if (!firstName || !lastName || !email || !password || !phone) {
+      setError("Ro‘yxatdan o‘tish uchun barcha maydonlarni to‘ldiring.");
       return;
     }
 
-    // Ma'lumotlarni localStorage-ga saqlash
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
+    const userData = { firstName, lastName, email, password, phone };
 
-    alert(isRegister ? "Registered successfully!" : "Logged in successfully!");
-    setUsername("");
-    setPassword("");
-    setError("");
+    try {
+      const response = await fetch("http://10.10.3.9:8080/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+
+      // if (response.status !== 201) {
+      //   const errorData = await response.json();
+      //   throw new Error(errorData.message || "Ro‘yxatdan o‘tishda xatolik.");
+      // }
+
+      setopen(true);
+
+      // alert("Ro‘yxatdan o‘tish muvaffaqiyatli amalga oshirildi!");
+      // handleToggle();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleSubmitLogin = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Iltimos, email va parolni kiriting.");
+      return;
+    }
+
+    const loginData = { email, password };
+
+    try {
+      const response = await fetch("http://10.10.3.9:8080/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.status !== 201) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Login muvaffaqiyatsiz.");
+      }
+
+      const data = await response.json();
+      const token = data?.data?.Authorize;
+      if (token) {
+        localStorage.setItem("authToken", token);
+        alert("Kirish muvaffaqiyatli amalga oshirildi!");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-        <div className="flex gap-3 mb-6">
-          <button
-            onClick={handleToggle}
-            className={`${
-              !isRegister
-                ? "text-blue-600 border-b-2 border-blue-600"
-                : "text-gray-600"
-            }`}
-          >
-            Sign in
-          </button>
-          <button
-            onClick={handleToggle}
-            className={`${
-              isRegister
-                ? "text-blue-600 border-b-2 border-blue-600"
-                : "text-gray-600"
-            }`}
-          >
-            Register
-          </button>
-        </div>
-
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Username or Email"
-              className="w-full outline-none px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 placeholder-gray-400"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mb-4 relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              className="w-full outline-none px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 placeholder-gray-400"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button
-              type="button"
-              className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
-            </button>
-          </div>
-
-          {isRegister && (
-            <div className="mb-4 relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Repeat Password"
-                className="w-full outline-none px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 placeholder-gray-400"
-              />
+        {open ? (
+          <>
+            kodni kiriting
+            <input />
+          </>
+        ) : (
+          <>
+            <div className="flex gap-3 mb-6">
+              <button
+                onClick={handleToggle}
+                className={
+                  !isRegister
+                    ? "text-blue-600 border-b-2 border-blue-600"
+                    : "text-gray-600"
+                }
+              >
+                Kirish
+              </button>
+              <button
+                onClick={handleToggle}
+                className={
+                  isRegister
+                    ? "text-blue-600 border-b-2 border-blue-600"
+                    : "text-gray-600"
+                }
+              >
+                Ro‘yxatdan o‘tish
+              </button>
             </div>
-          )}
-
-          {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 flex justify-center items-center"
-          >
-            {isRegister ? "Register" : "Login"} <span className="ml-2">→</span>
-          </button>
-        </form>
-
-        <div className="flex items-center my-6">
-          <hr className="flex-grow border-gray-300" />
-          <span className="mx-4 text-gray-500">OR</span>
-          <hr className="flex-grow border-gray-300" />
-        </div>
-
-        <div className="flex gap-10">
-          <button className="flex items-center justify-center border border-sky-400 text-sky-400 py-3 w-[230px] rounded-lg hover:bg-sky-200 hover:text-white">
-            <span className="mr-2">
-              <FaFacebookF />
-            </span>
-            Login with Facebook
-          </button>
-          <button className="flex items-center justify-center border border-red-400 text-red-400 py-3 w-[230px] rounded-lg hover:bg-red-200 hover:text-white">
-            <span className="mr-2">
-              <FaGoogle />
-            </span>
-            Login with Google
-          </button>
-        </div>
+            {isRegister ? (
+              <form onSubmit={handleSubmitRegister}>
+                <input
+                  type="text"
+                  placeholder="Ismingiz"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <input
+                  type="text"
+                  placeholder="Familiyangiz"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Parol"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <input
+                  type="text"
+                  placeholder="Telefon"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 text-white py-2"
+                >
+                  Ro‘yxatdan o‘tish
+                </button>
+              </form>
+            ) : (
+              <form onSubmit={handleSubmitLogin}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Parol"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full mb-4 px-4 py-2 border rounded-lg"
+                />
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 text-white py-2"
+                >
+                  Kirish
+                </button>
+              </form>
+            )}
+          </>
+        )}
+        {error && <p className="text-red-500 mt-4">{error}</p>}
       </div>
     </div>
   );
